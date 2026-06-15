84c25b7e9b210e18595e6c4c68c4da2b510618cb
Ian Simpson <thefamilysimpson@gmail.com>
2026-06-15 08:43:14 +0000

    card sort

--- _SDD/2026-27 1D Arrays of Records/gameboard.py
+++ _SDD/2026-27 1D Arrays of Records/gameboard.py
@@ -7,17 +7,20 @@ class square():
 
 def displayBoard(gameboard):
     for row in range(len(gameboard)):
-        print(str(gameboard[row][col].position) + ', ')
+        print(str(gameboard[row].position) + ', ')
     print()
 
 def initPosition(gameboard):
     # task 2
     # write code to set position of each square here
+    for index in range(len(gameboard)):                # <-----------------------------------------
+        gameboard[index].position = index + 1          # <-----------------------------------------
     return gameboard
 
 # initialisation
 
 # setup 1-D array of records (gameboard) here - task 1
+gameboard = [square() for index in range(50)]          # <-----------------------------------------
 
 displayBoard(gameboard)
 gameboard = initPosition(gameboard) # task 2
